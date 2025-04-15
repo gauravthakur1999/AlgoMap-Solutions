418939f3c68c70517b325daa5e4b863bfef886ea
gauravrthakur1999 <gauravrajuthakur@gmail.com>
2025-04-15 09:03:25 -0400

    Implement Trie (Prefix Tree)

---
+++ 8. Trees/14. Implement Trie (Prefix Tree).py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False  # Marks end of a full word
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children: # this will traverse the trie 
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord # if the flag for the curr TrieNode is true it is the last element 
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True  # Don't need endOfWord for prefix
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
